277ede1eabc4cdfe95a63648af3396aac5269c28
dcaschili <danielecaschili@tiscali.it>
2025-11-15 23:04:02 +0100

    avoid assert when not boomer

--- LevelIngredients/wall_of_death.gd
+++ LevelIngredients/wall_of_death.gd
@@ -7,8 +7,8 @@ func _on_body_entered(body: Node3D) -> void:
 		body.linear_velocity = Vector3.ZERO
 		character_died.emit(body)
 		var b = body as boomer
-		assert(b.player_idx >= 0 and b.player_idx < GameInstance.player_states.size())
-		GameInstance.player_states[b.player_idx].player_instance.queue_free() # Kill shooter
+		if b:
+			assert(b.player_idx >= 0 and b.player_idx < GameInstance.player_states.size())
+			GameInstance.player_states[b.player_idx].player_instance.queue_free() # Kill shooter
+			GameInstance.character_death.emit(b.player_idx)
 		body.queue_free() # Kill character
-		GameInstance.character_death.emit(b.player_idx)
-		
