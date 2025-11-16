827da9957dfde141a39c309bb6593e99eb7f7768
pikumb94 <pikumb94@gmail.com>
2025-11-16 15:46:01 +0100

    fix

--- Core/Garbage_Projectile.gd
+++ Core/Garbage_Projectile.gd
@@ -4,13 +4,11 @@ class_name projectile_garbage
 var impulse_scale : float = -100
 var attached_to : RigidBody3D
 
-@onready var sprite_3d: Sprite3D = $Sprite3D
-
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("Boomer"):
 		body.apply_central_impulse(Vector3(0,impulse_scale,0))
 		%Timer.start()
-		if !body.is_queued_for_deletion():
+		if is_instance_valid(body):#!body.is_queued_for_deletion():
 			attached_to = body.random_body_part()
 
 func _process(delta: float) -> void:
